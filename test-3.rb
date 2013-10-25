43cd5e9c8ce84fa960c8e8e2219b254ee55b7aa6
sai83 <sai.praneeth@gmail.com>
2013-10-24 22:15:22 -0500

    commited spell check..

--- test-3.rb
+++ test-3.rb
@@ -1,7 +1,7 @@
 require "rubygems"
 require "rspec"
 require "watir-webdriver"
-reqire "headless"
+require "headless"
  
 describe "www.google.com" do
   let(:browser) { @browser ||= Watir::Browser.new :firefox } 
